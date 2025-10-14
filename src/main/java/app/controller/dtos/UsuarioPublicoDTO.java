41fad33699b670515cfa7ba603dc87b689fbff18
lgareffi <luciagareffi@gmail.com>
2025-10-14 13:29:21 -0300

    agrego atributos a usuario publico DTO

--- src/main/java/app/controller/dtos/UsuarioPublicoDTO.java
+++ src/main/java/app/controller/dtos/UsuarioPublicoDTO.java
@@ -6,12 +6,18 @@ public class UsuarioPublicoDTO {
     private Long idUsuario;
     private String nombre;
     private String apellido;
+    private String email;
+    private String telefono;
+    private String fotoPerfilUrl;
 
     public UsuarioPublicoDTO(Usuarios u) {
         super();
         this.idUsuario = u.getIdUsuario();
         this.nombre = u.getNombre();
         this.apellido = u.getApellido();
+        this.email = u.getMail();
+        this.telefono = u.getTelefonoCelular();
+        this.fotoPerfilUrl = u.getProfilePicUrl();
     }
 
     public UsuarioPublicoDTO() {
@@ -30,4 +36,39 @@ public class UsuarioPublicoDTO {
         return apellido;
     }
 
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFotoPerfilUrl() {
+        return fotoPerfilUrl;
+    }
+
+    public void setFotoPerfilUrl(String fotoPerfilUrl) {
+        this.fotoPerfilUrl = fotoPerfilUrl;
+    }
 }
